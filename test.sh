2a9cb608d1a82e676c90fa9ab0bd7dc6e83152b9
BEQSONA-cmd <tvildiani2001@gmail.com>
2023-12-14 21:53:27 +0100

    Cabbinette

--- test.sh
+++ test.sh
@@ -1,9 +1,28 @@
-mv Makefile testing
-mv main.c testing.c
-cp ~/Cabinette/main.c .
-cp ~/Cabinette/Makefile .
-make test
-rm main.c
-rm Makefile
-mv testing Makefile
-mv testing.c main.c
+file1="libft.h"
+file2="get_next_line.c"
+
+if [ -e "$file1" ]; then
+    mv Makefile testing
+    mv main.c testing.c
+    cp ~/Cabinette/main.c .
+    cp ~/Cabinette/Makefile .
+    make test
+    rm main.c
+    rm Makefile
+    mv testing Makefile
+    mv testing.c main.c
+elif [ -e "$file2"]; then
+    mv Makefile testing
+    mv main.c testing.c
+    cp ~/Cabinette/main1.c .
+    cp ~/Cabinette/Makefile1 .
+    mv main1.c main.c
+    mv Makefile1 Makefile
+    make test
+    rm main.c
+    rm Makefile
+    mv testing Makefile
+    mv testing.c main.c
+else
+  echo "File does not exist."
+fi
