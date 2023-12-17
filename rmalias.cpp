734fb85771b47455f06cc8018da70dbb9c93a3ba
BEQSONA-cmd <tvildiani2001@gmail.com>
2023-12-17 13:20:38 +0100

    CABINETTE[4]

--- rmalias.cpp
+++ rmalias.cpp
@@ -4,27 +4,7 @@
 #include <vector>
 #include <algorithm>
 
-void ft_putstr_fd(const char* s, std::ostream& os)
-{
-    os << s;
-}
-
-void markLines(const char* filename, const std::vector<std::string>& linesToAdd) {
-    std::ofstream file(filename, std::ios::out | std::ios::app);
-
-    if (!file.is_open()) {
-        perror("Error opening the file");
-        exit(1);
-    }
-
-    for (const auto& line : linesToAdd) {
-        ft_putstr_fd(line.c_str(), file);
-    }
-
-    file.close();
-}
-
-void removeMarkedLines(const char* filename, const std::vector<std::string>& linesToRemove) {
+void removeSpecificLines(const char* filename, const std::vector<std::string>& linesToRemove) {
     std::ifstream inFile(filename);
     if (!inFile.is_open()) {
         perror("Error opening the file");
@@ -35,7 +15,15 @@ void removeMarkedLines(const char* filename, const std::vector<std::string>& lin
     std::string line;
 
     while (std::getline(inFile, line)) {
-        if (std::find(linesToRemove.begin(), linesToRemove.end(), line) == linesToRemove.end()) {
+        bool found = false;
+        for (size_t i = 0; i < linesToRemove.size(); ++i) {
+            if (line == linesToRemove[i]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
             lines.push_back(line);
         }
     }
@@ -48,8 +36,8 @@ void removeMarkedLines(const char* filename, const std::vector<std::string>& lin
         exit(1);
     }
 
-    for (const auto& l : lines) {
-        outFile << l << "\n";
+    for (size_t i = 0; i < lines.size(); ++i) {
+        outFile << lines[i] << "\n";
     }
 
     outFile.close();
@@ -67,16 +55,14 @@ int main() {
     char path[1024];
     snprintf(path, sizeof(path), "%s/%s", home, filename);
 
-    std::vector<std::string> linesToRemove = 
-    {
-        "alias cabinette=~/Cabinette/test.sh",
-        "",
-        "alias paulo=~/Cabinette/test.sh",
-        "",
-        "alias C_update=~/Cabinette/update",
-    };
+    std::vector<std::string> linesToRemove;
+    linesToRemove.push_back("alias cabinette=~/Cabinette/test.sh");
+    linesToRemove.push_back("");
+    linesToRemove.push_back("alias paulo=~/Cabinette/test.sh");
+    linesToRemove.push_back("");
+    linesToRemove.push_back("alias C_update=~/Cabinette/update");
 
-    removeMarkedLines(path, linesToRemove);
+    removeSpecificLines(path, linesToRemove);
 
     return 0;
 }
