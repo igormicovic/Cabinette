6d02bcaf4f37ef0a8b32763756297dfdad001d94
BEQSONA-cmd <tvildiani2001@gmail.com>
2023-12-22 19:08:02 +0100

    Cabinette[4]

---
+++ Libft/rmalias.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+void removeLines(const char* filename, const std::vector<std::string>& linesToRemove) {
+    std::ifstream inFile(filename);
+    if (!inFile.is_open()) {
+        perror("Error opening the file");
+        exit(1);
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+
+    while (std::getline(inFile, line)) {
+        if (std::find(linesToRemove.begin(), linesToRemove.end(), line) == linesToRemove.end()) {
+            lines.push_back(line);
+        }
+    }
+
+    inFile.close();
+
+    std::ofstream outFile(filename);
+    if (!outFile.is_open()) {
+        perror("Error opening the file");
+        exit(1);
+    }
+
+    for (std::vector<std::string>::const_iterator it = lines.begin(); it != lines.end(); ++it) {
+        outFile << *it << "\n";
+    }
+
+    outFile.close();
+}
+
+int main() {
+    const char* filename = "Makefile";
+
+    std::vector<std::string> linesToRemove;
+    linesToRemove.push_back("test: $(NAME)");
+    linesToRemove.push_back("\tmake all");
+    linesToRemove.push_back("\tmake clean");
+    linesToRemove.push_back("\tmake fclean");
+    linesToRemove.push_back("\tmake re");
+    linesToRemove.push_back("\t$(CC) $(CFLAGS) $(NAME) main.c");
+    linesToRemove.push_back("\tmake clean");
+    linesToRemove.push_back("\tclear");
+    linesToRemove.push_back("\t./a.out");
+
+    removeLines(filename, linesToRemove);
+
+    return 0;
+}
--- test.sh
+++ test.sh
@@ -5,37 +5,44 @@ file4="get_next_line.c"
 file5="ft_printf.h"
 
 if [ -e "$file1" ]; then
-  mv Makefile testing
   mv main.c testing.c
   cp ~/Cabinette/Libft/main.c .
-  cp ~/Cabinette/Libft/Makefile .
+  cp ~/Cabinette/Libft/alias.cpp .
+  cp ~/Cabinette/Libft/rmalias.cpp .
+  g++ alias.cpp && ./a.out
   make test
+  g++ rmalias.cpp && ./a.out
+  rm a.out
+  rm rmalias.cpp
+  rm alias.cpp
   rm main.c
-  rm Makefile
-  mv testing Makefile
   mv testing.c main.c
 elif [ -e "$file2" ]; then
-  mv Makefile testing
   mv main.c testing.c
   cp ~/Cabinette/Libft/main.c .
-  cp ~/Cabinette/Libft/Makefile1 .
-  mv Makefile1 Makefile
+  cp ~/Cabinette/Libft/alias.cpp .
+  cp ~/Cabinette/Libft/rmalias.cpp .
+  g++ alias.cpp && ./a.out
   make test
+  g++ rmalias.cpp && ./a.out
+  rm a.out
+  rm rmalias.cpp
+  rm alias.cpp
   rm main.c
-  rm Makefile
-  mv testing Makefile
   mv testing.c main.c
 elif [ -e "$file3" ]; then
-  mv Makefile testing
   mv main.c testing.c
   cp ~/Cabinette/Libft/main2.c .
-  cp ~/Cabinette/Libft/Makefile2 .
-  mv Makefile2 Makefile
+  cp ~/Cabinette/Libft/alias.cpp .
+  cp ~/Cabinette/Libft/rmalias.cpp .
   mv main2.c main.c
+  g++ alias.cpp && ./a.out
   make test
+  g++ rmalias.cpp && ./a.out
+  rm a.out
+  rm rmalias.cpp
+  rm alias.cpp
   rm main.c
-  rm Makefile
-  mv testing Makefile
   mv testing.c main.c
 elif [ -e "$file4" ]; then
   mv Makefile testing
